df96c29e4f6957f296dfab754f5f5246bf17c4a4
Aurimas <aurisjunevicius@gmail.com>
2024-01-05 01:36:54 +0200

    Lepton ne default rezimu inicializacija

--- FusionVisionCode/CM7/Core/Src/lepton_app.c
+++ FusionVisionCode/CM7/Core/Src/lepton_app.c
@@ -9,6 +9,8 @@
 #include "debug_api.h"
 #include "circular_buffer.h"
 
+extern I2C_HandleTypeDef hi2c4;
+
 static const uint8_t test_data[] = {
 		0x10,0x0,
 		0x0, 0x0,0x0, 0x1,0x0, 0x2,0x0, 0x3,0x0, 0x4,0x0, 0x5,0x0, 0x6,0x0, 0x7,0x0, 0x8,0x0, 0x9,0x0, 0xa,0x0, 0xb,0x0, 0xc,0x0, 0xd,0x0, 0xe,0x0, 0xf,0x0, 0x10,0x0, 0x11,0x0, 0x12,0x0, 0x13,0x0, 0x14,0x0, 0x15,0x0, 0x16,0x0, 0x17,0x0, 0x18,0x0, 0x19,0x0, 0x1a,0x0, 0x1b,0x0, 0x1c,0x0, 0x1d,0x0, 0x1e,0x0, 0x1f,0x0, 0x20,0x0, 0x21,0x0, 0x22,0x0, 0x23,0x0, 0x24,0x0, 0x25,0x0, 0x26,0x0, 0x27,0x0, 0x28,0x0, 0x29,0x0, 0x2a,0x0, 0x2b,0x0, 0x2c,0x0, 0x2d,0x0, 0x2e,0x0, 0x2f,0x0, 0x30,0x0, 0x31,0x0, 0x32,0x0, 0x33,0x0, 0x34,0x0, 0x35,0x0, 0x36,0x0, 0x37,0x0, 0x38,0x0, 0x39,0x0, 0x3a,0x0, 0x3b,0x10,0x1,
@@ -516,6 +518,63 @@ static uint32_t test_data_index = 0;
  * Video output - RAW14
  * GPIO - VSYNC enabled
  */
+
+#define LEPTON_I2C_Address 0x2A
+#define AGC_ID 0x1
+#define System_ID 0x02
+#define Video_ID 0x03
+#define OEM_ID 0x48
+#define Radiometry_ID 0x0E
+
+bool Lepton_AGC_Enable(void){
+	{
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, AGC_ID, 1, 10);
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x01, 1, 10); //AGC Set Enable Command
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x00, 1, 10); //Disable AGC - False
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x01, 1, 10); //Enable AGC - True
+	}
+	return true;
+}
+
+bool Lepton_AGC_Mode(void){
+	{
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, AGC_ID, 1, 10);
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x05, 1, 10); //ACG Set Mode Command
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x01, 1, 10); //Linear AGC- True
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x00, 1, 10); //HEQ AGC - False
+	}
+	return true;
+}
+
+bool Telemetry_Enable(void){
+	{
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, System_ID, 1, 10);
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x19, 1, 10); //Telemetry Enable Command
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x00, 1, 10); //Telemetry Disabled - False
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x01, 1, 10); //Telemetry Enabled - True
+	}
+	return true;
+}
+
+bool Telemetry_Location(void){
+	{
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, System_ID, 1, 10);
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x19, 1, 10); //Telemetry Enable Command
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x01, 1, 10); //Telemetry Location Header - True
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x00, 1, 10); //Telemetry Location Footer - False
+	}
+	return true;
+}
+
+bool GPIO_VSYNC(void){
+	{
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, OEM_ID, 1, 10);
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x55, 1, 10); //OEM GPIO Mode Command
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x00, 1, 10); //GPIO Mode GPIO - False
+		HAL_I2C_Master_Transmit(&hi2c4, LEPTON_I2C_Address, 0x01, 1, 10); //GPIO Mode VSYNC - True
+	}
+	return true;
+}
 
 
 /* GPIO
