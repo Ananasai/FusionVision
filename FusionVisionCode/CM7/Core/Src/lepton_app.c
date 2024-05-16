5bda8637dfe418632b05f8bc89ffff726e208635
simonuks221 <simonasriauka@gmail.com>
2024-05-16 19:12:01 +0300

    feat: decoded segments working

--- FusionVisionCode/CM7/Core/Src/lepton_app.c
+++ FusionVisionCode/CM7/Core/Src/lepton_app.c
@@ -11,8 +11,8 @@
 #include "fonts.h"
 #include "lepton_api.h"
 
-#pragma GCC push_options
-#pragma GCC optimize ("O3") //TODO: remove this
+//#pragma GCC push_options
+//#pragma GCC optimize ("O3") //TODO: remove this
 
 #define __DEBUG_FILE_NAME__ "LPT"
 
@@ -45,7 +45,6 @@
  * GPIO - VSYNC enabled
  */
 
-
 /* GPIO
  * VSYNC - HIGH impulse when new frame generated
  * SPI4_CS - active low
@@ -56,15 +55,12 @@ static bool rx_byte_flag = false;
 static bool continue_rx_flag = false;
 
 static uint8_t *rx_buffer;
-static uint8_t rx_buffer1[CIRC_BUF_LEN] = {0}; //TODO: shorten?
-static uint8_t rx_buffer2[CIRC_BUF_LEN] = {0}; //TODO: shorten?
+static uint8_t rx_buffer1[CIRC_BUF_LEN] = {0};
+static uint8_t rx_buffer2[CIRC_BUF_LEN] = {0};
 
 static sCircularBuffer_t circ_buffer;
 
 static uint16_t *image_buffer = {0};
-
-static uint32_t pixel_index = 0;
-static bool packet_left_side = false;
 
 static uint8_t calculated_segment = 0;
 static uint8_t calculated_packet = 0;
@@ -116,10 +112,38 @@ bool Lepton_APP_Start(uint16_t *new_image_buffer){
 	//if(Lepton_API_SetGpio() == false){
 	//	error("Failed set GPIO\r\n");
 	//}
-
 	return true;
 }
 
+static inline void DrawLine(uint8_t segment, uint8_t packet) {
+	segment--;
+	uint8_t line = 122 - (30 * segment) - ((uint8_t)(packet / 2));
+	uint8_t collumn_start = (packet % 2) == 0 ? 80 : 0;
+	for(uint8_t i = 0; i < 80; i++) {
+		*(image_buffer + i + line * SCREEN_WIDTH + collumn_start) = 0xFFFF;
+	}
+}
+
+static inline void DrawLineBuffer(uint8_t segment, uint8_t packet, uint8_t *line_buffer) {
+	segment--;
+	uint8_t line = 122 - (30 * segment) - ((uint8_t)(packet / 2));
+	uint8_t collumn_start = (packet % 2) == 0 ? 80 : 0;
+	for(uint8_t i = 0; i < 80; i++) {
+		//uint16_t colour = *(line_buffer + i*2) + *(line_buffer + i*2 + 1) * 256;
+		uint16_t colour = *(line_buffer + i*2) >> 3;
+		*(image_buffer + (80 - i) + line * SCREEN_WIDTH + collumn_start) = colour;
+	}
+}
+
+static inline void DrawPixel(uint8_t segment, uint8_t packet, uint8_t pixel_idx) {
+	segment--;
+	uint8_t line = 122 - (30 * segment) - ((uint8_t)(packet / 2));
+	uint8_t collumn_start = (packet % 2) == 0 ? 80 : 0;
+	*(image_buffer + line * SCREEN_WIDTH + collumn_start + (80 - pixel_idx)) = 0xFFFF;
+
+}
+
+uint16_t decoded_segment = 0;
 void Lepton_APP_Run(uint8_t *flag){
 	if(rx_byte_flag){
 		rx_byte_flag = false;
@@ -131,52 +155,81 @@ void Lepton_APP_Run(uint8_t *flag){
 
 		/* Decode data */
 		uint16_t decoded_packet = rx_buffer[1];
-		uint16_t decoded_segment = (rx_buffer[0] >> 4) & 0x0F;
 		if(decoded_packet == 20){
+			decoded_segment = (rx_buffer[0] >> 4) & 0x0F;
 			if(decoded_segment == 0){
 				return;
 			} else if(decoded_segment > 4){
 				return;
 			}else{
+				debug("Seg: %hu\r\n", decoded_segment);
 				//calculated_segment = decoded_segment - 1; //Sometime gives 200+
 			}
 		}
-		if(decoded_packet > 60){
+		if(decoded_packet >= 60){
 			return;
 		}
+
+
+		//
 		//TODO: could be different with telemetry enabled
 		//uint16_t pixel_index = curr_segment_index * SEGMENT_PIXEL_AMOUNT + curr_packet_index * PACKET_PIXEL_AMOUNT;
 		/* Is packet first in row or second */
-		//debug("Pck: %d\r\n", decoded_packet);
-		packet_left_side = decoded_packet % 2 == 0;
-		uint16_t row = ((3 - calculated_segment) * 30 + ((59- decoded_packet) / 2));//*2;
-		uint16_t collumn = packet_left_side ? 80 : 0; /* 240 - middle of screen, 3*80*/
-		pixel_index = row * SCREEN_WIDTH + collumn;
-		/* Get every other pixel as AGC is on */
+
+		/*
+		 * DrawLine(1, 0);
+			DrawLine(1, 2);
+			DrawLine(1, 5);
+			DrawLine(2, 0);
+			DrawLine(3, 0);
+			DrawLine(4, 0);
+			DrawLine(4, 59);
+			DrawPixel(4, 58, 0);
+			uint8_t colour_buffer[160] = {0};
+			memset(colour_buffer, 0xFF, 80);
+			memset(colour_buffer + 80, 0x11, 80);
+			DrawLineBuffer(4, 56, colour_buffer);
+		 */
+		//DrawLineBuffer(calculated_segment, decoded_packet, rx_buffer + 2);
+		/*
+		 * segment--;
+			uint8_t line = 120 - (30 * segment) - ((uint8_t)(packet / 2));
+			uint8_t collumn_start = (packet % 2) == 0 ? 80 : 0;
+			for(uint8_t i = 0; i < 80; i++) {
+				//uint16_t colour = *(line_buffer + i*2) + *(line_buffer + i*2 + 1) * 256;
+				uint16_t colour = *(line_buffer + i*2) >> 3;
+				*(image_buffer + (80 - i) + line * SCREEN_WIDTH + collumn_start) = colour;
+			}
+		 * */
+		uint16_t row = 119 - (30 * calculated_segment) - ((uint8_t)(decoded_packet / 2));
+		uint16_t collumn_start = decoded_packet % 2 == 0 ? 80 : 0;
+		uint32_t pixel_index = row * SCREEN_WIDTH + collumn_start;
+
+//		for(uint16_t i = 0; i < 80; i++) {
+//			*(image_buffer + pixel_index + (80 - i)) = rx_buffer[2 + i*2];
+//		}
+
 		for(uint16_t i = PACKET_DATA_LEN + 2; i > 3; i -= 2){
 			if(pixel_index > 480*320){
-				error("OUT OF BOUNDS row: %d, col %d, seg %d, pack %d\r\n", row, collumn, calculated_segment, decoded_packet);
+				error("OUT OF BOUNDS row: %d, col %d, seg %d, pack %d\r\n", row, collumn_start, calculated_segment, decoded_packet);
+				continue;
+			}
+			if(decoded_segment == 0) {
 				continue;
 			}
 			*(image_buffer + pixel_index) = rx_buffer[i];
-			//*(image_buffer + pixel_index + 1) = rx_buffer[i];
-			//*(image_buffer + pixel_index + 2) = rx_buffer[i];
-			//if(row > 1){
-				//*(image_buffer - SCREEN_WIDTH + pixel_index) = rx_buffer[i];
-				//*(image_buffer - SCREEN_WIDTH + pixel_index + 1) = rx_buffer[i];
-				//*(image_buffer - SCREEN_WIDTH + pixel_index + 2) = rx_buffer[i];
-			//}
-			//pixel_index += 3;
 			pixel_index += 1;
 		}
+
 		calculated_packet++;
 		if(calculated_packet == PACKET_IN_SEGMENT){
+			decoded_segment = 0;
 			calculated_segment++;
 			calculated_packet = 0;
 			if(calculated_segment == 4){
 				calculated_segment = 0;
 				/* Start drawing  */
-				debug("Done\r\n");
+				//debug("Done\r\n");
 				*flag = 0x01;
 				HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
 				return;
@@ -196,6 +249,9 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim){
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef * hspi){
     if(hspi == &hspi4){
     	HAL_GPIO_WritePin(SPI4_CS_GPIO_Port, SPI4_CS_Pin, GPIO_PIN_SET);
+    	if(rx_byte_flag == true) {
+    		HardFault_Handler();
+    	}
     	rx_byte_flag = true;
     }
 }
@@ -206,4 +262,4 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 		HardFault_Handler(); //TODO: not working
 	}
 }
-#pragma GCC pop_options
+//#pragma GCC pop_options
